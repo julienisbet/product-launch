daf2a17d1c8b9a25899ec6270780e29e7aaec5a7
Julie Nisbet <jmnisbet@gmail.com>
2022-02-15 18:23:14 -0800

    adding hide / remove logic to the form

---
+++ app.js
@@ -0,0 +1,23 @@
+// console.log("hello world!");
+
+// const myDiv = document.createElement("div");
+// myDiv.textContent = "Hello world!";
+
+// document.body.append(myDiv);
+
+const form = document.getElementById("contact-form");
+console.log(form);
+
+const email = document.getElementById("email");
+email.value = "info@example.com";
+
+const thankYouDiv = document.getElementById("thank-you");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("i am submitting the form!");
+  // remove the class hide from the thank you div
+  thankYouDiv.classList.remove("hide");
+  // add the class hide to the form
+  form.classList.add("hide");
+});
